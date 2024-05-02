f256bf0ebd1d0e8ce6a932cd163fc36cc7134a1c
Remi-Debu <remi.debu@free.fr>
2024-05-02 13:17:00 +0200

    update bissextile

--- bissextile/bissext.cbl
+++ bissextile/bissext.cbl
@@ -1,15 +1,23 @@
+      ******************************************************************
+      *    Le programme demande à l'utilisateur de saisir une année    *
+      *    et affiche si l'année est bissextile ou non.                *
+      *    Puis demande à l'utilisateur s'il souhaite continuer.       *
+      ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. bissext.
+       AUTHOR.     Rémi.
        
+      ****************************************************************** 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  WS-YEAR   PIC 9(4).
+       01  WS-YEAR   PIC 9(04).
        01  WS-RESULT PIC X(40).
 
        01  WS-CONTINUE PIC X(03) VALUE "Y".
            88 WS-YES VALUE "Y".
            88 WS-NO  VALUE "N".
        
+      ****************************************************************** 
        PROCEDURE DIVISION.
            PERFORM UNTIL WS-NO
            DISPLAY "Saisir une annee :" SPACE WITH NO ADVANCING 
