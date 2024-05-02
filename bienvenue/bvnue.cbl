c14d0a3b5c5738177408ebac008587783cdf136d
Remi-Debu <remi.debu@free.fr>
2024-05-02 13:17:12 +0200

    update bienvenue

--- bienvenue/bvnue.cbl
+++ bienvenue/bvnue.cbl
@@ -1,22 +1,21 @@
       ******************************************************************
-      *                                                                *
       *    Le programme demande à l'utilisateur de saisir son nom      *
       *    et l'affiche avec un message de bienvenue.                  *
-      *                                                                *
       ******************************************************************
-
        IDENTIFICATION DIVISION.
        PROGRAM-ID. bvnue.
        AUTHOR.     Rémi.
 
+      ******************************************************************
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  WS-NOM PIC X(20).
+       01  WS-NAME PIC X(20).
 
+      ******************************************************************
        PROCEDURE DIVISION.
-           DISPLAY "Saisir votre nom :" SPACE NO ADVANCING.
-           ACCEPT WS-NOM.
+           DISPLAY "Saisir votre nom :" SPACE WITH NO ADVANCING.
+           ACCEPT WS-NAME.
 
-           DISPLAY "Bienvenue," SPACE FUNCTION TRIM(WS-NOM) SPACE "!".
+           DISPLAY "Bienvenue," SPACE FUNCTION TRIM(WS-NAME) SPACE "!".
 
            STOP RUN.
