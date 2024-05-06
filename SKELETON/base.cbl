7ee52fa5ab681b76e89b83ec304697968efc176c
Remi-Debu <remi.debu@free.fr>
2024-05-06 16:47:20 +0200

    update base skeleton

--- SKELETON/base.cbl
+++ SKELETON/base.cbl
@@ -8,7 +8,7 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT F-NAME ASSIGN TO "filename.txt"
+           SELECT F-INPUT ASSIGN TO "filename.txt"
            ORGANIZATION IS LINE SEQUENTIAL
            ACCESS MODE IS SEQUENTIAL
            FILE STATUS IS FS-INPUT.
@@ -16,17 +16,38 @@
       ******************************************************************
        DATA DIVISION.
        FILE SECTION.
-       FD  F-NAME
+       FD  F-INPUT
            RECORD CONTAINS 100 CHARACTERS
            RECORDING MODE IS F.
        01  R-NAME PIC X(100).
 
        WORKING-STORAGE SECTION.
        01  FS-INPUT PIC X(02).
-           88 FS-DEPT-OK VALUE "00".
-           88 FS-DEPT-EOF VALUE "10".
+           88 FS-INPUT-OK VALUE "00".
+           88 FS-INPUT-EOF VALUE "10".
 
       ****************************************************************** 
        PROCEDURE DIVISION.
-           GOBACK.
+      ******************************************************************
+       START-R-IP.
+           OPEN INPUT F-INPUT.
+           IF FS-INPUT EQUAL "00"
+              SET FS-INPUT-OK TO TRUE
+
+              PERFORM UNTIL FS-INPUT-EOF
+                 READ F-INPUT 
+                 AT END 
+                    SET FS-INPUT-EOF TO TRUE
+                 NOT AT END 
+    
+                  END-READ
+              END-PERFORM
+           ELSE
+              DISPLAY "ERREUR :" SPACE FS-INPUT
+           END-IF.
+           CLOSE F-INPUT.
+       END-R-IP.
+           EXIT.
+
+           STOP RUN.
            
