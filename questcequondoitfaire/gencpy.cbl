f903686c3af5082753d05076f8758ef3004e6aac
Remi-Debu <remi.debu@free.fr>
2024-05-07 02:03:02 +0200

    update copy generator

--- questcequondoitfaire/gencpy.cbl
+++ questcequondoitfaire/gencpy.cbl
@@ -1,4 +1,6 @@
       ******************************************************************
+      *    Le programme permet de lire un fichier et à partir de ce    *
+      *    fichier générer un copybook composer uniquement de filler   *
       ****************************************************************** 
        IDENTIFICATION DIVISION.
        PROGRAM-ID. gencpy.
@@ -10,7 +12,6 @@
        FILE-CONTROL.
            SELECT F-INPUT ASSIGN 
            TO "COBOL_FICHIER_MODELE_POUR_CLAUSE_COPY_Exercice.txt"
-      *    TO "test.txt"
            ORGANIZATION IS LINE SEQUENTIAL
            ACCESS MODE IS SEQUENTIAL
            FILE STATUS IS FS-INPUT.
@@ -24,9 +25,9 @@
        DATA DIVISION.
        FILE SECTION.
        FD  F-INPUT
-           RECORD CONTAINS 5 TO 1000 CHARACTERS
+           RECORD CONTAINS 1 TO 1000 CHARACTERS
            RECORDING MODE IS V.
-       01  R-INPUT PIC X(230).
+       01  R-INPUT PIC X(219).
 
        FD  F-OUTPUT
            RECORD CONTAINS 80 CHARACTERS
@@ -35,42 +36,46 @@
 
        WORKING-STORAGE SECTION.
        01  FS-INPUT PIC X(02).
-           88 FS-INPUT-OK VALUE "00".
+           88 FS-INPUT-OK  VALUE "00".
            88 FS-INPUT-EOF VALUE "10".
 
        01  FS-OUTPUT PIC X(02).
            88 FS-OUTPUT-OK VALUE "00".
 
-       01  PHRASE.
-           03 M-CNT PIC 9(04) VALUE 1.
-           03 TABLE-MOT OCCURS 1 TO 1000 TIMES
-                     DEPENDING ON M-CNT
-                     INDEXED BY M-IDX.
-               05 MOT PIC X(40).
-               05 FILLER-LENGTH PIC 9(02).
-               05 MOT-LENGTH PIC 9(02) VALUE 1.
+       01  INPUT-LINE.
+           03 W-CNT PIC 9(04) VALUE 1.
+           03 WORD-ARRAY OCCURS 1 TO 1000 TIMES
+                     DEPENDING ON W-CNT
+                     INDEXED BY W-IDX.
+               05 WORD        PIC X(40).
+               05 WORD-LENGTH PIC 9(02).
+               05 SEP-LENGTH  PIC 9(02).
 
        01  R-IDX              PIC 9(04) VALUE 1.
-       01  M-POS              PIC 9(04) VALUE 1.
-       01  R-LENGTH           PIC 9(05).
-      *01  R-SPACES           PIC 9(05).
-      *01  N-MOT              PIC 9(03).
 
-       01  PRINT.
-           03 START-FILLER PIC X(27) 
+       01  CB-FORMAT.
+           03 CB-GROUP PIC X(16) 
+           VALUE "       01 GROUP.".
+           03 CB-START-FILLER  PIC X(27) 
            VALUE "           03 FILLER PIC X(".
+           03 CB-VALUE-FILLER  PIC X(08) 
+           VALUE ") VALUE ".
+           03 CB-SPACES-FILLER PIC X(07) 
+           VALUE "SPACES.".
 
       ****************************************************************** 
        PROCEDURE DIVISION.
        START-MAIN.
            PERFORM START-R-IP THRU END-R-IP.
+           PERFORM START-W-OP THRU END-W-OP.
        END-MAIN.
            STOP RUN.
 
       ******************************************************************
+      *    Lis le fichier input et appel le paragraphe
+      ******************************************************************
        START-R-IP.
-           OPEN INPUT F-INPUT
-                OUTPUT F-OUTPUT.
+           OPEN INPUT F-INPUT.
            IF FS-INPUT EQUAL "00"
               SET FS-INPUT-OK TO TRUE
                
@@ -79,10 +84,8 @@
                  AT END 
                     SET FS-INPUT-EOF TO TRUE
                  NOT AT END 
-                    PERFORM START-SPLIT-PHRASE-LOOP 
-                       THRU END-SPLIT-PHRASE-LOOP
-
-                    PERFORM START-W-OP THRU END-W-OP
+                    PERFORM START-SPLIT-INPUT-LINE 
+                       THRU END-SPLIT-INPUT-LINE
                   END-READ
               END-PERFORM
            ELSE
@@ -93,56 +96,74 @@
            EXIT.
 
       ******************************************************************
-       START-SPLIT-PHRASE-LOOP.
-           INSPECT R-INPUT TALLYING R-LENGTH FOR CHARACTERS.
-      *    INSPECT R-INPUT TALLYING R-SPACES FOR ALL SPACES.
+      *    Parcours chaque caractères du string R-INPUT pour stocker   *
+      *    chaque mot dans le tableau "WORD-ARRAY" ainsi que le nombre *
+      *    d'espaces qui sépare ces mots.                              *        
+      ******************************************************************
+       START-SPLIT-INPUT-LINE.
+           PERFORM VARYING R-IDX FROM 1 BY 1 
+                   UNTIL R-IDX > FUNCTION LENGTH(R-INPUT)
+       
+      *       SI le caractères est un espace
+      *       ajoute 1 au nombre d'espaces
+      *       ET SI le prochain caractères n'est pas un espace
+      *       ajoute 1 au compteur de mot
+              IF R-INPUT(R-IDX:1) EQUAL SPACE 
+                 ADD 1 TO SEP-LENGTH(W-CNT)
 
-           PERFORM VARYING R-IDX FROM 1 BY 1 UNTIL R-IDX > R-LENGTH
-               IF R-INPUT(R-IDX:1) NOT EQUAL SPACE
-                  MOVE R-INPUT(R-IDX:1) 
-                  TO MOT(M-CNT)(MOT-LENGTH(M-CNT):1)
-           
-                  ADD 1 TO MOT-LENGTH(M-CNT)
-               ELSE 
-                   ADD 1 TO FILLER-LENGTH(M-CNT)
-                   SET M-POS TO 1
-                   ADD 1 TO M-CNT
-               END-IF
+                 IF R-INPUT(R-IDX + 1:1) NOT EQUAL SPACE
+                    ADD 1 TO W-CNT
+                 END-IF
+
+      *       SINON le caractère n'est pas un espace
+      *       ajoute 1 à la longueur du mot et
+      *       ajoute le caractère au mot dans le tableau
+              ELSE 
+                 ADD 1 TO WORD-LENGTH(W-CNT)
+
+      *          MOVE "le caractère"
+                 MOVE R-INPUT(R-IDX:1) 
+      *          TO "le mot du tableau à la position qui dépend de la
+      *          longueur du mot actuelle"
+                 TO WORD(W-CNT)(WORD-LENGTH(W-CNT):1)
+              END-IF
            END-PERFORM.
-       END-SPLIT-PHRASE-LOOP.
+       END-SPLIT-INPUT-LINE.
            EXIT.
 
       ******************************************************************
+      *    Ecris le copybook en fonction du contenu du tableau de mots *
+      ******************************************************************
        START-W-OP.
            OPEN OUTPUT F-OUTPUT.
-           MOVE "       01 GROUPE." TO R-OUTPUT.
-           WRITE R-OUTPUT.
 
-           PERFORM VARYING M-IDX FROM 1 BY 1 UNTIL M-IDX > M-CNT
-               IF MOT(M-IDX) NOT EQUAL SPACE
-                   INITIALIZE R-OUTPUT
-                   SUBTRACT 1 FROM MOT-LENGTH(M-IDX)
+      *    Ecris le 01 GROUP
+           WRITE R-OUTPUT FROM CB-GROUP.
 
-                   STRING START-FILLER 
-                   MOT-LENGTH(M-IDX) ") VALUE" SPACE 
-                   "'"FUNCTION TRIM(MOT(M-IDX)) "'."
-                   DELIMITED BY SIZE
-                   INTO R-OUTPUT
-               
-                   WRITE R-OUTPUT 
-
-                   INITIALIZE R-OUTPUT
-                   STRING START-FILLER 
-                   FILLER-LENGTH(M-IDX) ") VALUE SPACES."
-                   DELIMITED BY SIZE
-                   INTO R-OUTPUT
-               
-                   WRITE R-OUTPUT
-                    
-      *            ADD 1 TO N-MOT
-               END-IF
+           PERFORM VARYING W-IDX FROM 1 BY 1 UNTIL W-IDX >= W-CNT
+              IF WORD(W-IDX) NOT EQUAL SPACE
+                 INITIALIZE R-OUTPUT
+       
+      *          Ecris le filler contenant le mot
+                 STRING CB-START-FILLER 
+                        WORD-LENGTH(W-IDX) 
+                        CB-VALUE-FILLER 
+                    "'" FUNCTION TRIM(WORD(W-IDX)) "'."
+                 DELIMITED BY SIZE
+                 INTO R-OUTPUT
+                 WRITE R-OUTPUT 
+       
+      *          Ecris le filler de sépartion contenant que des SPACES    
+                 INITIALIZE R-OUTPUT
+                 STRING CB-START-FILLER 
+                        SEP-LENGTH(W-IDX) 
+                        CB-VALUE-FILLER
+                        CB-SPACES-FILLER
+                 DELIMITED BY SIZE
+                 INTO R-OUTPUT
+                 WRITE R-OUTPUT
+              END-IF
            END-PERFORM.
-
            CLOSE F-OUTPUT.
        END-W-OP.
            EXIT.
--- questcequondoitfaire/output.cpy
+++ questcequondoitfaire/output.cpy
@@ -1,37 +1,37 @@
-       01 GROUPE.
+       01 GROUP.
            03 FILLER PIC X(13) VALUE 'IdentifiantCT'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(05) VALUE SPACES.
            03 FILLER PIC X(04) VALUE 'Muna'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(03) VALUE SPACES.
            03 FILLER PIC X(13) VALUE 'IdentifiantSE'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(04) VALUE SPACES.
            03 FILLER PIC X(06) VALUE 'MunaSE'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(04) VALUE SPACES.
            03 FILLER PIC X(13) VALUE 'IdentifiantRG'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(03) VALUE SPACES.
            03 FILLER PIC X(06) VALUE 'MunaRG'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(04) VALUE SPACES.
            03 FILLER PIC X(08) VALUE 'Libellé'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(15) VALUE SPACES.
            03 FILLER PIC X(07) VALUE 'MotClé'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(14) VALUE SPACES.
            03 FILLER PIC X(08) VALUE 'Lissable'.
            03 FILLER PIC X(01) VALUE SPACES.
            03 FILLER PIC X(03) VALUE 'TVA'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(02) VALUE SPACES.
            03 FILLER PIC X(03) VALUE 'CMU'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(03) VALUE SPACES.
            03 FILLER PIC X(04) VALUE 'Etat'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(02) VALUE SPACES.
            03 FILLER PIC X(06) VALUE 'TypeRG'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(04) VALUE SPACES.
            03 FILLER PIC X(08) VALUE 'Soustype'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(06) VALUE SPACES.
            03 FILLER PIC X(07) VALUE 'DateMAJ'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(05) VALUE SPACES.
            03 FILLER PIC X(11) VALUE 'DébutEffet'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(02) VALUE SPACES.
            03 FILLER PIC X(08) VALUE 'FinEffet'.
-           03 FILLER PIC X(01) VALUE SPACES.
+           03 FILLER PIC X(04) VALUE SPACES.
            03 FILLER PIC X(09) VALUE 'Priorité'.
            03 FILLER PIC X(01) VALUE SPACES.
