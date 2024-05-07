fcd3a9c2bcd366ed623045f6d5709028bf84bccf
Remi-Debu <remi.debu@free.fr>
2024-05-07 11:38:31 +0200

    update bac a sable

--- bac-sable/syracuse/syrac.cbl
+++ bac-sable/syracuse/syrac.cbl
@@ -11,9 +11,10 @@
        DATA DIVISION.
       ****************************************************************** 
        WORKING-STORAGE SECTION.
-       01  WS-NUM PIC 9(05).
+       01  WS-NUM    PIC 9(05).
+       01  WS-Z-NUM  PIC Z(04)9.
        01  WS-RESULT PIC 9(10).
-       01  WS-REST PIC 9(10).
+       01  WS-REST   PIC 9(10).
            
 
       ****************************************************************** 
@@ -22,22 +23,35 @@
            DISPLAY "Saisir un nombre entier :" SPACE WITH NO ADVANCING.
            ACCEPT WS-NUM.
            DISPLAY SPACE.
+           DISPLAY "Details des calculs effectues :".
 
            PERFORM UNTIL WS-NUM = 1
            DIVIDE WS-NUM BY 2 GIVING WS-RESULT REMAINDER WS-REST
 
       *    Si nombre est pair
            IF WS-REST EQUAL 0 
-              DISPLAY WS-NUM SPACE "/ 2 =" SPACE WITH NO ADVANCING
+              INITIALIZE WS-Z-NUM
+              MOVE WS-NUM TO WS-Z-NUM
+              DISPLAY FUNCTION TRIM(WS-Z-NUM) SPACE "/ 2 =" 
+              SPACE WITH NO ADVANCING
+
               DIVIDE WS-NUM BY 2 GIVING WS-NUM
-              DISPLAY WS-NUM
+              INITIALIZE WS-Z-NUM
+              MOVE WS-NUM TO WS-Z-NUM
+              DISPLAY FUNCTION TRIM(WS-Z-NUM)
 
       *    Si nombre impair
            ELSE
-              DISPLAY WS-NUM SPACE "x 3 + 1 =" SPACE WITH NO ADVANCING
+              INITIALIZE WS-Z-NUM
+              MOVE WS-NUM TO WS-Z-NUM
+              DISPLAY FUNCTION TRIM(WS-Z-NUM) SPACE "x 3 + 1 =" 
+              SPACE WITH NO ADVANCING
+
               MULTIPLY WS-NUM BY 3 GIVING WS-NUM
               ADD 1 TO WS-NUM
-              DISPLAY WS-NUM
+              INITIALIZE WS-Z-NUM
+              MOVE WS-NUM TO WS-Z-NUM
+              DISPLAY FUNCTION TRIM(WS-Z-NUM)
            END-IF
 
       *    DISPLAY WS-NUM
