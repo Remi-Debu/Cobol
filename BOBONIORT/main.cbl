a1c77a4391b8d723d2dfa29692c1b7eef799ca42
Remi-Debu <remi.debu@free.fr>
2024-05-28 19:06:35 +0200

    commit

---
+++ BOBONIORT/main.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      ******************************************************************
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. main.
+       AUTHOR.     Rémi.
+
+      ******************************************************************
+
+       PROCEDURE DIVISION.
+       0000-START-MAIN.
+           CALL "yourSubprogramName".
+
+           DISPLAY "HELLO COBOL".
+       END-0000-MAIN.
+           STOP RUN.
