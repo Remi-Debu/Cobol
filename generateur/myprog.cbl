b9ad2f68eee2e762b69fa5ac8b43700623ee7a82
Remi-Debu <remi.debu@free.fr>
2024-05-28 19:06:06 +0200

    update program generator

--- generateur/myprog.cbl
+++ generateur/myprog.cbl
@@ -5,11 +5,23 @@
        PROGRAM-ID. myprog.
        AUTHOR.   YourName.
 
+       EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+       01  DBNAME   PIC  X(30) VALUE "dbname".
+       01  USERNAME PIC  X(30) VALUE "username".
+       01  PASSWD   PIC  X(10) VALUE "password".
+       EXEC SQL END DECLARE SECTION END-EXEC.
+
+       EXEC SQL INCLUDE SQLCA END-EXEC.
+
 
       ******************************************************************
 
        PROCEDURE DIVISION.
        0000-START-MAIN.
-           CALL "yourSubprogramName".
+           EXEC SQL
+              CONNECT :USERNAME IDENTIFIEDBY :PASSWD USING :DBNAME
+           END-EXEC.
+           DISPLAY "HELLO COBOL".
        END-0000-MAIN.
+           EXEC SQL DISCONNECT ALL END-EXEC.
            GOBACK.
